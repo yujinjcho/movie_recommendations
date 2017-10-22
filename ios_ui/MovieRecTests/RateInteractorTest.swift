f6d0e10539d50ec4143020f825943e6e738d41c5
Yujin Cho <yujinjcho@gmail.com>
2017-10-21 19:34:47 -0700

    add unit test for RateInteractor

---
+++ ios_ui/MovieRecTests/RateInteractorTest.swift
@@ -0,0 +1,67 @@
+//
+//  RateInteractorTest.swift
+//  MovieRecTests
+//
+//  Created by Yujin Cho on 10/21/17.
+//  Copyright © 2017 Yujin Cho. All rights reserved.
+//
+
+import XCTest
+@testable import MovieRec
+
+class RateInteractorTest: XCTestCase {
+    
+    var dataManager = MockRateDataManager()
+    var interactor : RateInteractor?
+    var output = MockRatePresenter()
+    
+    
+    override func setUp() {
+        super.setUp()
+        interactor = RateInteractor(dataManager: dataManager)
+        if let interactor = interactor {
+            interactor.output = output
+        } else {
+            XCTFail("interactor should be initialized")
+        }
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    func testStoreRating() {
+        if let interactor = interactor {
+            interactor.storeRating(ratingType: "1")
+            XCTAssertTrue(dataManager.storeRatingCalled, "should call storeRating")
+            XCTAssertTrue(dataManager.removeFirstMovieCalled, "should call removeFirstMovie")
+            XCTAssertTrue(dataManager.loadCurrentMovieCalled, "should call loadCurrentMovie")
+            XCTAssertTrue(output.presentCurrentMovieCalled, "should call presentCurrentMovie")
+            
+        } else {
+            XCTFail("interactor should be initialized")
+        }
+    }
+    
+    func testShowCurrentMovie() {
+        let movie = MovieModel(title: "test_movie", photoUrl: "http://www.test.com", movieId: "1")
+        if let interactor = interactor {
+            interactor.showCurrentMovie(currentMovie: movie)
+            XCTAssertTrue(output.presentCurrentMovieCalled, "should call presentCurrentMovie")
+        } else {
+            XCTFail("interactor should be initialized")
+        }
+    }
+    
+    func testInitializeDataManager() {
+        if let interactor = interactor {
+            interactor.initializeDataManager()
+            XCTAssertTrue(dataManager.loadRatingsCalled, "should call loadRatings")
+            XCTAssertTrue(dataManager.loadMoviesCalled, "should call loadMovies")
+            XCTAssertTrue(output.presentCurrentMovieCalled, "should call presentCurrentMovie")
+        } else {
+            XCTFail("interactor should be initialized")
+        }
+    }
+}
